0be8ff69ac3dc814348ae7ca2ce77246c3b3f204
koa-inn <koainn@gmail.com>
2026-06-12 17:48:21 -0700

    chore(29.2-03): regenerate build artifacts (cache stamps + minified JS/CSS)
    - HTML cache stamps updated by npm run build (admin.html, brewpad.html, all pages)
    - js/admin.min.js, js/admin.js BUILD_TIMESTAMP refreshed
    - All other minified artifacts regenerated from npm run build

--- js/admin.js
+++ js/admin.js
@@ -4,7 +4,7 @@
   'use strict';
 
   // Build timestamp - updated on each deploy
-  var BUILD_TIMESTAMP = '2026-06-13T00:40:59.861Z';
+  var BUILD_TIMESTAMP = '2026-06-13T00:47:55.858Z';
   console.log('[Admin] Build: ' + BUILD_TIMESTAMP);
 
   var accessToken = null;
