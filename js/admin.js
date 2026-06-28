892f693a8479f7a5e320040e84c5fad119489027
koa-inn <koainn@gmail.com>
2026-06-28 10:31:11 -0700

    chore(44-06): rebuild kiosk.min.js + admin.min.js with gift card UI; update cache stamps
    - Regenerated js/kiosk.min.js and js/admin.min.js via npm run build (terser)
    - Updated BUILD_TIMESTAMP in js/admin.js
    - Updated ?v= cache-busting stamps on all HTML files
    - Confirmed: gift-card/issue present in both min bundles (grep -c returns 1 each)
    - npm test: 928 tests, 49 suites, all green

--- js/admin.js
+++ js/admin.js
@@ -4,7 +4,7 @@
   'use strict';
 
   // Build timestamp - updated on each deploy
-  var BUILD_TIMESTAMP = '2026-06-27T20:12:54.231Z';
+  var BUILD_TIMESTAMP = '2026-06-28T17:30:31.613Z';
   console.log('[Admin] Build: ' + BUILD_TIMESTAMP);
 
   var accessToken = null;
