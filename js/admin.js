a26353e70d93df4880ac414f69befee91ebdb68c
koa-inn <koainn@gmail.com>
2026-08-18 20:28:46 -0700

    build: stamp artifacts for staging-middleware frontend switch

--- js/admin.js
+++ js/admin.js
@@ -48,7 +48,7 @@
   }
 
   // Build timestamp - updated on each deploy
-  var BUILD_TIMESTAMP = '2026-08-18T21:25:39.847Z';
+  var BUILD_TIMESTAMP = '2026-08-19T03:28:31.369Z';
   console.log('[Admin] Build: ' + BUILD_TIMESTAMP); // eslint-disable-line no-console -- deploy build-verification log
 
   var accessToken = null;
